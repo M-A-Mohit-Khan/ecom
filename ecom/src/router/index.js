c01a1f3a13ae6f2b4e177ba277ab674516db9e78
M-A-Mohit-Khan <m.a.mohit.khan@gmail.com>
2023-05-02 23:35:47 +0600

    Second commit

---
+++ ecom/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Product from '../views/Product.vue'
+import Detail from '../views/Detail.vue'
+import Token from '../views/Token.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: Product
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: Detail
+    },
+    {
+      path: '/token',
+      name: 'token',
+      component: Token
+    },
+  ]
+})
+
+export default router
